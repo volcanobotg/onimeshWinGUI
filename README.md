37b95092295528bb9524d62ffd392a1391d7635a
luisfae <luis.fae@gmail.com>
2015-10-17 13:16:29 -0600

    New update
    Added gitignore file and removed unnecessary stuff, added instructions
    to readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # onimeshWinGUI
+
+This is the GUI for OniMesh(OM), which will be used mainly as a launcher for OM itself. It has ben developed using QT Creator 3.5.0 (opensource), with C++, based on QT 5.5.0. It's the latest version of QT since I installed it (September 27th, 2015).
+
+For information on QT, go to http://www.qt.io/ide/
+To download QT, access http://www.qt.io/download/ and state that you're developing Open source software in order to get the free opensource version. 
+
+Simply install it.
+
+Open QT and open the project in this repository.
+
+Once you've opened this project using QT, simply build it and run it. The GUI will appear. 
---
+++ omgui.cpp
@@ -0,0 +1,89 @@
+#include "omgui.h"
+#include "ui_omgui.h"
+#include <QFileDialog>
+#include <iostream>
+#include <stdlib.h>
+#include <QThread>
+#include <QtGui>
+#include <QProcess>
+
+using namespace std;
+
+bool fileSelected, dirSelected;
+QString dirName, fileName;
+QStringList fileNames;
+QString fileNamesSize, fileNamesStr="";
+
+OMGUI::OMGUI(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::OMGUI)
+{
+    ui->setupUi(this);
+}
+
+OMGUI::~OMGUI()
+{
+    delete ui;
+}
+
+void OMGUI::on_btnSelectFiles_clicked()
+{
+    //Creates the dialog to select different ONI files
+    QFileDialog dialog(this);
+    fileNames = dialog.getOpenFileNames(this, tr("Select ONI file(s)"), "", tr("ONI Files (*.oni)"));
+
+    //creates a String with the file names and locations for the prompt to show
+    QString allFiles = "";
+    for (int i = 0; i< fileNames.length(); i++){
+        allFiles.append("File selected:" + fileNames.at(i) + " \n");
+        fileNamesStr.append(fileNames.at(i) + ",");
+    }
+    fileNamesStr.remove(fileNamesStr.size());
+    fileNamesSize = QString::number(fileNames.size());
+    ui->labelPrompt->setText(allFiles);
+
+
+    fileSelected = true;
+
+}
+
+void OMGUI::on_btnSelectDir_clicked()
+{
+    //Creates the dialog to select the directory
+    QFileDialog dialog;
+    dialog.setFileMode(QFileDialog::Directory);
+    dialog.setOption(QFileDialog::ShowDirsOnly);
+    int result = dialog.exec();
+
+    //If the result is true, the path for the directory is stored and it is shown in the prompt.
+    if (result){
+        dirName = dialog.selectedFiles()[0];
+        ui->labelPrompt->setText("Directory selected: " + dirName);
+        dirSelected = true;
+    }
+
+}
+
+void OMGUI::on_btnLaunch_clicked()
+{
+    if (fileSelected == true && dirSelected == true){
+        ui->labelPrompt->setText("Launching program!!!");
+
+        QProcess process;
+        process.startDetached("cmd.exe", QStringList() << "/k" << "echo Directory name: "<< dirName << "Files: " << fileNamesSize << "Names: " << fileNamesStr << "\"");
+
+        this->close();
+    }
+
+    //Tests if the ONI Files and/or the directory have been selected, and warns the user what they need to do prior to launching the program
+    else{
+        if (fileSelected == true){
+            ui->labelPrompt->setText("Select a directory first");
+        } else if (dirSelected == true){
+            ui->labelPrompt->setText("Load a .ONI file first");
+        } else{
+            ui->labelPrompt->setText("Select a directory and load a .ONI file first");
+        }
+
+    }
+}
