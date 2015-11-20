04b45035bc8bbb16c3a0bb712851c02a0f4526f5
chrispadgett <chris.e.padgett@gmail.com>
2015-11-20 13:11:25 -0700

    Added BSD License
    Added BSD License

--- omgui.h
+++ omgui.h
@@ -1,3 +1,12 @@
+/*
+* Copyright (c) 2015-, Christopher Padgett, David Andrews, Luis Henrique Fae Barboza
+*
+* You may use, distribute and modify this code under the terms of the BSD license.
+*
+* You should have received a copy of the full BSD license with this file.
+* If not, please visit: https://github.com/volcanobotg/onimesh for full license information.
+*/
+
 #ifndef OMGUI_H
 #define OMGUI_H
 
