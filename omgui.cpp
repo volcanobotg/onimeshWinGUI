84a0beb0078ba4d19f098862a006c1f83a181cb5
chrispadgett <chris.e.padgett@gmail.com>
2015-11-20 13:34:20 -0700

    Fixed License link
    Fixed License link

--- omgui.cpp
+++ omgui.cpp
@@ -4,7 +4,7 @@
 * You may use, distribute and modify this code under the terms of the BSD license.
 *
 * You should have received a copy of the full BSD license with this file.
-* If not, please visit: https://github.com/volcanobotg/onimesh for full license information.
+* If not, please visit: https://github.com/volcanobotg/onimeshWinGUI for full license information.
 */
 
 #include "omgui.h"
